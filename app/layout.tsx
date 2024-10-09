033dffcc979479463a74080b5c89966eb9fe6e86
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2024-10-09 16:43:41 +0530

    Add styles to the table

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,13 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
       <body>
         <NavBar />
         {children}
